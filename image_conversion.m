804f3157ec145bed67f31561ed62588c37019c03
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-10-01 01:04:21 -0700

    added comments

--- image_conversion.m
+++ image_conversion.m
@@ -1,19 +1,24 @@
+%% Loading image and renaming the variables
 %# Load the image data
 load trees;           
 % change the naming of the variables
 treeImage = X;
 treeColorMap = map;
 
+%% Matlab image conversion
 %# Convert indexed to grayscale
 matlabInd2Gray = ind2gray(treeImage, treeColorMap); 
 %# Compute an appropriate threshold
 treshholdLevel = graythresh(matlabInd2Gray);  
 matlabIm2bw = im2bw(matlabInd2Gray, treshholdLevel);
 
-customInd2Gray = custom_ind2gray(treeImage, treeColorMap);
-customIm2bw = custom_im2bw(customInd2Gray, treshholdLevel);
 
-%// Show colour image as well as resulting gray image
+%% Custom image conversion
+customInd2Gray = custom_ind2gray(treeImage, treeColorMap);
+customTreshholLevel = 0.4980;
+customIm2bw = custom_im2bw(customInd2Gray, customTreshholLevel);
+
+%% Show the results
 figure; 
 subplot(2,2,1); imshow(matlabInd2Gray); title('matlabInd2Gray');
 subplot(2,2,2); imshow(customInd2Gray); title('customInd2Gray');
