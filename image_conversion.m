8e9cefaab82591b9f0d2f14a28fe26c032d71e19
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-10-01 01:46:06 -0700

    added dummy psnr calculation

--- image_conversion.m
+++ image_conversion.m
@@ -5,17 +5,23 @@ treeImage = X;
 treeColorMap = map;
 
 % Convert indexed to grayscale
-mGray = ind2gray(treeImage, treeColorMap); 
+matlabGray = ind2gray(treeImage, treeColorMap); 
 % Compute an appropriate threshold
-treshholdLevel = graythresh(mGray);  
-matlabIm2bw = im2bw(mGray, treshholdLevel);
+treshholdLevel = graythresh(matlabGray);  
+matlabIm2bw = im2bw(matlabGray, treshholdLevel);
 
 customInd2Gray = custom_ind2gray(treeImage, treeColorMap);
 customTreshholLevel = 0.4980;
 customIm2bw = custom_im2bw(customInd2Gray, customTreshholLevel);
 
 figure; 
-subplot(2,2,1); imshow(mGray);title('matlabInd2Gray');
+subplot(2,2,1); imshow(matlabGray);title('matlabInd2Gray');
 subplot(2,2,2); imshow(customInd2Gray);title('customInd2Gray');
 subplot(2,2,3); imshow(matlabIm2bw);title('matlabIm2bw');
 subplot(2,2,4); imshow(customIm2bw);title('customIm2bw');
+
+% Calculate snr values of matlab
+[ps1, snr] = psnr(customInd2Gray, matlabGray);
+fprintf('\n The matlabGray SNR value is %0.4f \n', snr);
+
+
